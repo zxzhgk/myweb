0f75a26315c7541f98d7faf252a57c1cb6779750
zhangxu <zhangxu@ybm100.com>
2019-10-23 12:11:26 +0800

    聊天室搭建

---
+++ chat/src/plugins/axios.js
@@ -0,0 +1,68 @@
+"use strict";
+
+import Vue from 'vue';
+import axios from "axios";
+// import { getToken } from '@/utils/auth';
+
+// Full config:  https://github.com/axios/axios#request-config
+// axios.defaults.baseURL = process.env.baseURL || process.env.apiUrl || '';
+// axios.defaults.headers.common['Authorization'] = AUTH_TOKEN;
+// axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
+
+let config = {
+  baseURL: process.env.VUE_APP_BASE_API,
+  timeout: 60 * 1000, // Timeout
+  withCredentials: true, // Check cross-site Access-Control
+};
+
+const _axios = axios.create(config);
+
+_axios.interceptors.request.use(
+  config => {
+    // Do something before request is sent
+    config.headers['token'] = getToken('user_type');
+    // console.log(config)
+    // if(config.method == 'post') {
+    //   config.headers['Content-Type'] = 'application/x-www-form-urlencoded';
+    // }
+    return config;
+  },
+  error => {
+    // Do something with request error
+    return Promise.reject(error);
+  }
+);
+
+// Add a response interceptor
+_axios.interceptors.response.use(
+  response =>{
+    // Do something with response data
+    // console.log(response)
+    return response;
+  },
+  error => {
+    // Do something with response error
+    return Promise.reject(error);
+  }
+);
+
+Plugin.install = function(Vue, options) {
+  Vue.axios = _axios;
+  window.axios = _axios;
+  Object.defineProperties(Vue.prototype, {
+    axios: {
+      get() {
+        return _axios;
+      }
+    },
+    $axios: {
+      get() {
+        return _axios;
+      }
+    },
+  });
+};
+
+Vue.use(Plugin)
+
+export default Plugin;
